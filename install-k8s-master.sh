f5bee9159ed94aa3a8cf2d58567d0009cf1d96d5
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-08-20 01:04:28 +0900

    Update

---
+++ install-k8s-master.sh
@@ -0,0 +1,7 @@
+kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
