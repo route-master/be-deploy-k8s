915359ce7dda632fdb0e5d36c6c668c94516380d
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-08-20 02:16:22 +0900

    Update

--- install-k8s-common.sh
+++ install-k8s-common.sh
@@ -9,8 +9,8 @@ EOF
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/kubernetes.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/kubernetes.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list
 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
